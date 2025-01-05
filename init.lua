f041a5b9ed7fe0ee8f74ce3b4bac411830145329
Roshan Pisharody <r@shanp.fyi>
2025-01-05 19:48:59 +0530

    Added oil.nvim plugin

--- init.lua
+++ init.lua
@@ -847,6 +847,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.oil',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
